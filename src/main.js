319c595a344b98d5e928e8c33f87582fd2080b84
wf <weifei2696021231@163.com>
2021-08-27 07:51:00 +0800

    忘记换分支

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import store from './store'
 import 'lib-flexible/flexible'
 
 
+import { Button, List, Cell, Tabbar, TabbarItem, Icon } from 'vant'
+Vue.use(Button)
+Vue.use(Cell)
+Vue.use(List)
+Vue.use(Tabbar).use(TabbarItem)
+Vue.use(Icon);
+
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,26 @@
 <template>
     <div class="login">
-        login
+        <header><van-icon name="arrow-left" /></header>
+        <div class="login-box">
+
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.login{
+    width: 750px;
+    header{
+        width: 750px;
+        height: 30px;
+        padding-left: 10px;
+        padding-top: 10px;
+        .van-icon{
+            font-size: 40px;
+        }
+    }
+    .login-box{
+        
+    }
+}
+</style>
